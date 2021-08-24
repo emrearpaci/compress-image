50a43f987dc35897be4e4ba81d310162d6e9daf0
Emre ARPACI <39678709+emrearpaci@users.noreply.github.com>
2021-08-24 17:39:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,41 +20,8 @@ function compressImage($source , $destination , $quality)
 	return $destination;
 }
 
-?>
-
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>File</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body>
-
-	<div class="container"><br /><br /><br /><br />
-		<form method="POST" action="index.php" enctype="multipart/form-data">
-			<input type="file" name="image" /><br />
-			<input type="hidden" name="username" value="emre arpacı" />
-			<button name="formsubmit" type="submit" class="btn btn-primary">Upload</button>
-		</form>
-	</div>
-
-</body>
-</html>
-
-<?php
-
-if(isset($_POST['formsubmit']))
-{
-	$target 	= basename($_FILES['image']['name']);
-	$file 		= $_FILES['image']['tmp_name'];
-	if(compressImage($file , $target , 50))
-	{
-		echo '<script>alert("uploaded");</script>';
-	}
-	else
-	{
-		echo '<script>alert("error");</script>';
-	}
-}
+$target 	= basename($_FILES['image']['name']);
+$file 		= $_FILES['image']['tmp_name'];
+compressImage($file , $target , 50);
 
 ?>
